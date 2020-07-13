7572e7060b3663f22e7e6f863f46d3fbfaf9c0c9
Leonam Souza <leonam@netquant.com.br>
2020-07-13 13:11:40 -0300

    Commit inicial

---
+++ src/main/java/com/emprestimo/emprestimoapp/EmprestimoappApplication.java
@@ -0,0 +1,13 @@
+package com.emprestimo.emprestimoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emprestimo/emprestimoapp/EmprestimoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emprestimo.emprestimoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
